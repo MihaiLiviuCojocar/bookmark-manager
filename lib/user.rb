069c2d291c59a7878aebe72665270ef49c408d68
MihaiLiviuCojocar <cmihailiviu@yahoo.com>
2014-02-22 15:01:49 +0000

    'sign in' feature added

--- lib/user.rb
+++ lib/user.rb
@@ -1,6 +1,7 @@
 require 'bcrypt'
 
 class User
+
   attr_reader :password
   attr_accessor :password_confirmation
 
@@ -15,11 +16,30 @@ class User
   validates_confirmation_of :password, :message => "Your passwords don't match"
 
 
-
-
   def password=(password)
     @password = password
     self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def self.authenticate(email, password)
+    # that's the user who is trying to sign in
+    user = first(:email => email)
+    # if this user exists and the password provided matches
+    # the one we have password_digest for, everything's fine
+    #
+    # The Password.new returns an object that overrodes the == method
+    # Instead of comparing two passwords directly
+    # which is impossible because we have only one-way hash
+    # the == method calculates the candidate password_digest from
+    # the password givven and compares it to the password_digest
+    # it was initialised with.
+    # So, to recap : THIS IS NOT A STRING COMPARISON
+    if user && BCrypt::Password.new(user.password_digest) == password
+      # return this user
+      user
+    else
+      nil
+    end
   end
 
 
